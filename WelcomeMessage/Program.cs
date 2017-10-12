4d618bcc3fc26979d358be456041e2fefaf40b4c
Jackson Ryan <Jackson.Ryan12@education.nsw.gov.au>
2017-10-12 12:39:32 +1100

    Made thot awareness program

---
+++ WelcomeMessage/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter name here: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Welcome Message for " + name);
+            Console.WriteLine("Click 'Yes' to open Message");
+            string Yes = Console.ReadLine();
+            Console.WriteLine("Thots are near. Be wary, " + name);
+
+            //wait at end
+            Console.ReadKey();
+        }
+    }
+}
