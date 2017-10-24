e9949e365a60e0c181e1ced3e94e9b9f5f33e9f1
Jackson Ryan <Jackson.Ryan12@education.nsw.gov.au>
2017-10-25 10:38:29 +1100

    guessing game

---
+++ GuessMyNumber/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessMyNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //setup stuff
+
+            int count = 0;
+            Random rnd = new Random();
+            int rand = rnd.Next(1, 101);
+            Console.WriteLine("Please enter your guess");
+            int guess = -1;
+
+            //while
+
+            while (guess != rand)
+            {
+                count++;
+                Console.Write("Guess:");
+                guess = int.Parse(Console.ReadLine());
+
+                // If, else and else if
+
+                if (guess < rand)
+                {
+                    Console.WriteLine("Too Low!");
+                }
+                else if (guess > rand)
+                {
+                    Console.WriteLine("Too High!");
+                }
+                else
+                {
+                    Console.WriteLine($"You got it! It took you {count} tries!");
+                    Console.ReadLine();
+                }
+            }
+            
+        }
+    }
+}
--- loops/Program.cs
+++ loops/Program.cs
@@ -19,8 +19,6 @@ namespace loops
             }
 
             Console.ReadLine();
-
-
             }
         }
     }
