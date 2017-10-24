8bd47ce74edde46ffae6c9431c172f14d007f263
Jackson Ryan <Jackson.Ryan12@education.nsw.gov.au>
2017-10-24 14:56:11 +1100

    loops

---
+++ loops/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press Enter to see loop");
+           int number = int.Parse(Console.ReadLine());
+           
+            for (int x = 1; x <= number; x++)
+            { 
+                Console.WriteLine(x);
+            }
+
+            Console.ReadLine();
+
+
+            }
+        }
+    }
+}
