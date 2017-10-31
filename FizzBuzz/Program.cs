6fe5715f3b6fd5186d8584998e4b438588bed515
Jackson Ryan <Jackson.Ryan12@education.nsw.gov.au>
2017-11-01 09:37:47 +1100

    fizzbuzz colour and beeps

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -16,29 +16,36 @@ namespace FizzBuzz
                 //FizzBuzz
                 if (i % 3 == 0 && i%5 == 0)
                 {
+                    Console.ForegroundColor = ConsoleColor.Blue;
                     Console.WriteLine("Fizz Buzz");
+                    Console.Beep(13000, 100);
                 }
 
                 //Fizz
                 else if (i % 3 == 0)
                 {
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Fizz");
+                    Console.Beep(5000, 100);
                 }
                     
                 //Buzz
                 else if (i % 5 == 0)
                 {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
                     Console.WriteLine("Buzz");
-                
+                    Console.Beep(8000, 100);
                 }
 
                 //Print (i) if not divisible by 3, 5, or 3 and 5
                 else
                 {
+                    Console.ForegroundColor = ConsoleColor.White;
                     Console.WriteLine(i);
                 }
             }
             //wait
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Press Any Button to Exit");
             Console.ReadKey();
         }
