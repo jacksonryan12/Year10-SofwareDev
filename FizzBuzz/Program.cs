2c8088ad8db8bff5c166d5c11bf50a4fce825658
Jackson Ryan <Jackson.Ryan12@education.nsw.gov.au>
2017-11-01 09:28:33 +1100

    fizzbuzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //100 loop
+            for (int i = 1; i <= 100; i++)
+            {
+                //FizzBuzz
+                if (i % 3 == 0 && i%5 == 0)
+                {
+                    Console.WriteLine("Fizz Buzz");
+                }
+
+                //Fizz
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                    
+                //Buzz
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                
+                }
+
+                //Print (i) if not divisible by 3, 5, or 3 and 5
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            //wait
+            Console.WriteLine("Press Any Button to Exit");
+            Console.ReadKey();
+        }
+    }
+}
