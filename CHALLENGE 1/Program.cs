a31e0b25e8ba46c31139bce224a0485ce9e4e8ba
Jackson Ryan <Jackson.Ryan12@education.nsw.gov.au>
2017-11-22 10:10:41 +1100

    challenge 1

---
+++ CHALLENGE 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHALLENGE_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input name
+            Console.WriteLine("Please enter your name:");
+            string name = Console.ReadLine();
+
+            //inpute age
+            Console.WriteLine("Please enter your age:");
+            string age = Console.ReadLine();
+
+            //use name and age
+            Console.WriteLine($"Welcome {name}, you look good for {age}!");
+            Console.ReadLine();
+        }
+    }
+}
