ba83c9778c1e9fe6334e1f4f943fb674495e0028
Jackson Ryan <Jackson.Ryan12@education.nsw.gov.au>
2017-11-21 14:54:25 +1100

    UNFINISHED CLASSES

---
+++ MakeAPerson/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeAPerson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("Uncle", "Phil", 55);
+            p.male;
+        }
+    }
+    class Person
+    {
+        private string firstname;
+        private string lastname;
+        private int age;
+        private bool male;
+
+        public Person(string firstname, string lastname, int age, bool male)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.age = age;
+            male = true;
+        }
+
+        public void male()
+        {
+            if (male)
+            {
+                Console.WriteLine($"{firstname} {lastname} is female");
+            }
+            else
+            {
+                male = true;
+            }
+        }
+    }
+}
+
